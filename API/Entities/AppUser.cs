a49e59aeb9e90f4d638e725df8a7af4583b0e0d5
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-21 20:38:33 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }        
+        public string UserNamcleare { get; set; }
+    }
+}
