e67e2c35ec90df900b1c2fec03a00016d83db0c4
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-24 21:16:23 +0530

    End of section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public IHostEnvironment _env { get; }
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try 
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment() ? new ApiException(context.Response.StatusCode,ex.Message,ex.StackTrace?.ToString())
+                                                    : new ApiException(context.Response.StatusCode,ex.Message,"Internal Server Error");
+                
+                var options = new JsonSerializerOptions
+                                  {
+                                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                                  };
+                var json = JsonSerializer.Serialize(response,options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next:() => {
         this.router.navigateByUrl('/members')
-      },
-      error: error => {
-        this.toastr.error(error.error);
       }
     });
   }
